90a2b6acdd4b390b1077655df9eaca231f3741b0
chidakiyo <chidakiyo@gmail.com>
2018-11-26 20:44:20 +0900

    http2試す

---
+++ go-http2/http2.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+func main() {
+
+	certFile, _ := filepath.Abs("server.crt")
+	keyFile, _ := filepath.Abs("server.key")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Protocol: %s\n", r.Proto)
+	})
+
+	err := http.ListenAndServeTLS(":3000", certFile, keyFile, nil)
+	if err != nil {
+		log.Printf("[ERROR] %s", err)
+	}
+
+}
