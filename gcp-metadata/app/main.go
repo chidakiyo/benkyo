4f557ea24276f118861b5735d8da5079b9e826aa
chidakiyo <chidakiyo@gmail.com>
2019-08-22 16:53:43 +0900

    gae go1.11 環境でmetadata取れるか✓

--- gcp-metadata/app/main.go
+++ gcp-metadata/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"cloud.google.com/go/compute/metadata"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/appengine"
@@ -15,6 +16,9 @@ func main() {
 	route.GET("/", root)
 	route.GET("/header", headers)
 	route.GET("/env", env)
+
+	// GAE上のmetadataを取得する
+	route.GET("/gcp", gcpMeta)
 
 	appengine.Main() // Listen
 }
@@ -42,4 +46,40 @@ func env(g *gin.Context) {
 	}
 
 	g.String(http.StatusOK, "ok")
+}
+
+func gcpMeta(g *gin.Context) {
+
+	ip, _ := metadata.ExternalIP()
+	fmt.Printf("IP: %s\n", ip)
+
+	host, _ := metadata.Hostname()
+	fmt.Printf("HOST: %s\n", host)
+
+	att, _ := metadata.InstanceAttributes()
+	fmt.Printf("Atts: %+v\n", att)
+
+	iid, _ := metadata.InstanceID()
+	fmt.Printf("IID: %s\n", iid)
+
+	name, _ := metadata.InstanceName()
+	fmt.Printf("NAME: %s\n", name)
+
+	iTag, _ := metadata.InstanceTags()
+	fmt.Printf("iTag: %+v\n", iTag)
+
+	iip, _ := metadata.InternalIP()
+	fmt.Printf("iIP: %s\n", iip)
+
+	npid, _ := metadata.NumericProjectID()
+	fmt.Printf("numPID: %s\n", npid)
+
+	pid, _ := metadata.ProjectID()
+	fmt.Printf("PID: %s\n", pid)
+
+	patt, _ := metadata.ProjectAttributes()
+	fmt.Printf("pATT: %+v\n", patt)
+
+	g.String(http.StatusOK, "end")
+
 }
--- k8s/drill/03/cmd/hello-server/main.go
+++ k8s/drill/03/cmd/hello-server/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(writer,"Hello Hogehoge!")
+		fmt.Fprint(writer, "Hello Hogehoge!")
 	})
 	log.Fatal(http.ListenAndServe(": 8080", nil))
 }
