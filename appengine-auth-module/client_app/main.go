43e615f4b2ee7de9b8e02890dd352975ae843079
Kiyokatsu Chida <chidakiyo@gmail.com>
2019-09-09 14:45:02 +0900

    middleware化してみたり

--- appengine-auth-module/client_app/main.go
+++ appengine-auth-module/client_app/main.go
@@ -14,49 +14,68 @@ func main() {
 	route := gin.Default()
 	http.Handle("/", route)
 
-	route.GET("/", func(context *gin.Context) {
-		// Get Project ID
-		projectId, err := metadata.ProjectID()
-		if err != nil {
-			context.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-
-		// Get ID Token
-		audience := os.Getenv("ID_TOKEN_AUDIENCE")
-		fmt.Printf("Audience: %s\n", audience)
-
-		idToken, err := metadata.Get("instance/service-accounts/default/identity?audience=" + audience)
-		if err != nil {
-			context.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-		fmt.Printf("ID Token: %s\n", idToken)
-
-		// Call backend service
-		client := &http.Client{}
-		req, err := http.NewRequest("GET", fmt.Sprintf("https://server-dot-%s.appspot.com", projectId), nil)
-		req.Header.Add("Authorization", "Bearer "+idToken)
-		if err != nil {
-			context.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-
-		resp, err := client.Do(req)
-		if err != nil {
-			context.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-		defer resp.Body.Close()
-
-		b, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			context.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-
-		context.String(http.StatusOK, "Response from backend:\n  %s", string(b))
-	})
-
+	route.GET("/", handle)
 	appengine.Main()
+}
+
+func handle(context *gin.Context) {
+
+	// ProjectID
+	projectId := getProjectID()
+
+	// audience
+	audience := os.Getenv("ID_TOKEN_AUDIENCE")
+	fmt.Printf("Audience: %s\n", audience)
+
+	// ID_Token
+	idToken := generateToken(audience)
+
+	// Call backend service
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://server-dot-%s.appspot.com", projectId), nil)
+	req.Header.Add("Authorization", "Bearer "+idToken)
+	if err != nil {
+		context.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		context.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		context.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	context.String(http.StatusOK, "Response from backend:\n  %s", string(b))
+}
+
+func generateToken(audience string) string {
+	idToken, err := metadata.Get("instance/service-accounts/default/identity?audience=" + audience)
+	if err != nil {
+		panic(err) // TODO 手抜き
+	}
+	fmt.Printf("ID Token: %s\n", idToken)
+	return idToken
+}
+
+// project id の取得
+// metaサーバから取得するより環境変数(GAEの場合）で取得したほうがパフォーマンス良さそうなので
+func getProjectID() string {
+	envProjID, ok := os.LookupEnv("PROJECT_ID")
+	if ok {
+		fmt.Printf("Project ID: %s (env)\n", envProjID)
+		return envProjID
+	}
+	projectId, err := metadata.ProjectID()
+	if err != nil {
+		panic(err) // TODO 手抜き
+	}
+	fmt.Printf("Project ID: %s (meta)\n", envProjID)
+	return projectId
 }
--- appengine-auth-module/server_app/main.go
+++ appengine-auth-module/server_app/main.go
@@ -4,10 +4,11 @@ import (
 	"cloud.google.com/go/compute/metadata"
 	"encoding/json"
 	"fmt"
-	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/appengine"
 	"net/http"
+	"os"
 	"regexp"
 )
 
@@ -15,81 +16,104 @@ func main() {
 	route := gin.Default()
 	http.Handle("/", route)
 
-	route.GET("/", func(context *gin.Context) {
-		// Fetch Authorization Header
-		bearerHeader := context.Request.Header.Get("Authorization")
-		if bearerHeader == "" {
-			context.AbortWithError(http.StatusUnauthorized, fmt.Errorf("No Authorization header found"))
-			return
-		}
-		fmt.Printf("# BearerHeader: %s\n", bearerHeader)
-
-		re := regexp.MustCompile(`^\s*Bearer\s+(.+)$`)
-		matched := re.FindStringSubmatch(bearerHeader)
-		if len(matched) != 2 {
-			context.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Authorization header is invalid format"))
-			return
-		}
-		fmt.Printf("# Matched Result: %v\n", matched)
-		bearerToken := matched[1]
-
-		// Verify ID Token
-		token, err := jwt.ParseWithClaims(bearerToken, &IdTokenClaims{}, func(token *jwt.Token) (interface{}, error) {
-			kid := token.Header["kid"].(string)
-			fmt.Printf("# kid: %s\n", kid)
-
-			// Get certificate
-			resp, err := http.Get("https://www.googleapis.com/oauth2/v1/certs")
-			if err != nil {
-				return nil, err
-			}
-			defer resp.Body.Close()
-			decoder := json.NewDecoder(resp.Body)
-			var jsonBody interface{}
-			if err := decoder.Decode(&jsonBody); err != nil {
-				return nil, err
-			}
-			cert := jsonBody.(map[string]interface{})[kid].(string)
-
-			fmt.Printf("# JsonBody: %+v\n", jsonBody)
-
-			return jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
-		})
-		if err != nil {
-			context.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid token: %s", err))
-			return
-		}
-		fmt.Printf("# token: %+v\n", token)
-		fmt.Printf("# token Claims: %+v\n", token.Claims)
-
-		claims, ok := token.Claims.(*IdTokenClaims)
-		if !(ok && token.Valid) {
-			context.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid token"))
-			return
-		}
-
-		// Get Project ID
-		projectId, err := metadata.ProjectID()
-		if err != nil {
-			fmt.Println(err)
-			context.String(http.StatusInternalServerError, "Error")
-			return
-		}
-
-		// Check if the request came from same application
-		if claims.Email != fmt.Sprintf("%s@appspot.gserviceaccount.com", projectId) {
-			context.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid token: email is invalid, %s\n", claims.Email))
-			return
-		}
-
-		fmt.Printf("# Claims: %#v\n", claims)
-		context.String(http.StatusOK, "Request by: %s", claims.Email)
-	})
-
+	route.GET("/", middleware, handle)
 	appengine.Main()
+}
+
+func middleware(g *gin.Context) {
+	// Fetch Authorization Header
+	bearerHeader := g.Request.Header.Get("Authorization")
+	if bearerHeader == "" {
+		g.AbortWithError(http.StatusUnauthorized, fmt.Errorf("No Authorization header found"))
+		g.Abort()
+		return
+	}
+	fmt.Printf("# BearerHeader: %s\n", bearerHeader)
+
+	re := regexp.MustCompile(`^\s*Bearer\s+(.+)$`)
+	matched := re.FindStringSubmatch(bearerHeader)
+	if len(matched) != 2 {
+		g.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Authorization header is invalid format"))
+		g.Abort()
+		return
+	}
+	fmt.Printf("# Matched Result: %v\n", matched)
+	bearerToken := matched[1]
+	g.Set("TOKEN", bearerToken) // set token.
+}
+
+func verifyToken(g *gin.Context, bearerToken string) (IdTokenClaims, bool) {
+	// Verify ID Token
+	token, err := jwt.ParseWithClaims(bearerToken, &IdTokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		kid := token.Header["kid"].(string)
+		fmt.Printf("# kid: %s\n", kid)
+
+		// Get certificate
+		resp, err := http.Get("https://www.googleapis.com/oauth2/v1/certs")
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		decoder := json.NewDecoder(resp.Body)
+		var jsonBody interface{}
+		if err := decoder.Decode(&jsonBody); err != nil {
+			return nil, err
+		}
+		cert := jsonBody.(map[string]interface{})[kid].(string)
+
+		fmt.Printf("# JsonBody: %+v\n", jsonBody)
+
+		return jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
+	})
+	if err != nil {
+		g.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid token: %s", err))
+		return IdTokenClaims{}, false
+	}
+	fmt.Printf("# token: %+v\n", token)
+	fmt.Printf("# token Claims: %+v\n", token.Claims)
+
+	claims, ok := token.Claims.(*IdTokenClaims)
+	if !(ok && token.Valid) {
+		g.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid token"))
+		return IdTokenClaims{}, false
+	}
+
+	projectID := getProjectID()
+	// Check if the request came from same application
+	if claims.Email != fmt.Sprintf("%s@appspot.gserviceaccount.com", projectID) {
+		g.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid token: email is invalid, %s\n", claims.Email))
+		return IdTokenClaims{}, false
+	}
+
+	return *claims, true
+}
+
+func handle(context *gin.Context) {
+
+	bearerToken, _ := context.Get("TOKEN")
+	claims, _ := verifyToken(context, bearerToken.(string))
+
+	fmt.Printf("# Claims: %#v\n", claims)
+	context.String(http.StatusOK, "Request by: %s", claims.Email)
 }
 
 type IdTokenClaims struct {
 	Email string `json:"email"`
 	jwt.StandardClaims
+}
+
+// project id の取得
+// metaサーバから取得するより環境変数(GAEの場合）で取得したほうがパフォーマンス良さそうなので
+func getProjectID() string {
+	envProjID, ok := os.LookupEnv("PROJECT_ID")
+	if ok {
+		fmt.Printf("Project ID: %s (env)\n", envProjID)
+		return envProjID
+	}
+	projectId, err := metadata.ProjectID()
+	if err != nil {
+		panic(err) // TODO 手抜き
+	}
+	fmt.Printf("Project ID: %s (meta)\n", envProjID)
+	return projectId
 }
