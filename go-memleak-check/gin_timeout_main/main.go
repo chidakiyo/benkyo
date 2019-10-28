27e5b30c6503417c559a17c000a127d58f5854b6
chidakiyo <chidakiyo@gmail.com>
2019-10-28 22:01:31 +0900

    timeout

--- go-memleak-check/gin_timeout_main/main.go
+++ go-memleak-check/gin_timeout_main/main.go
@@ -10,7 +10,8 @@ import (
 
 func main() {
 	route := gin.Default()
-	route.GET("long", func(g *gin.Context) {
+
+	route.GET("01", func(g *gin.Context) {
 		c, cancel := context.WithTimeout(g, 3*time.Second)
 		defer cancel()
 
@@ -31,6 +32,28 @@ func main() {
 
 		g.String(http.StatusOK, "success")
 	})
+
+	route.GET("02", func(i *gin.Context) {
+
+		ch := make(chan error, 1)
+
+		// main process
+		go func() {
+			time.Sleep(4 * time.Second)
+			ch <- nil // complete
+		}()
+
+		select {
+		case <-ch: // success
+			i.String(http.StatusOK, "complete") // success
+			return
+		case <-time.After(2 * time.Second): // timeout
+			i.String(http.StatusGatewayTimeout, "ごめんね")
+			return
+		}
+
+	})
+
 	http.Handle("/", route)
 	route.Run(":8080")
 }
