6760d748387525acdc2d27eb8d5ea74c660b5e11
chidakiyo <chidakiyo@gmail.com>
2019-11-05 22:15:33 +0900

    error周りの調査

--- go-memleak-check/lib/random_stream_test.go
+++ go-memleak-check/lib/random_stream_test.go
@@ -1,8 +1,40 @@
 package lib
 
-import "testing"
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
 
 func Test_gen_byte(t *testing.T) {
 	b := genBytes(8)
 	t.Logf("%v", b)
+}
+
+func Test_err_go13(t *testing.T) {
+
+	result1 := func() error {
+		return errors.New("new error")
+	} ()
+
+	result2 := func() error {
+		return nil
+	}()
+
+	result3 := func() error {
+		return fmt.Errorf("%w", error(nil))
+	}()
+
+	if result1 != nil {
+		t.Logf("result is error! %s", result1.Error())
+	}
+
+	if result2 != nil {
+		t.Logf("result2 is error! %s", result2.Error())
+	}
+
+	if result3 != nil {
+		t.Logf("result3 is error! %s", result3.Error()) // ホントは入らない
+	}
+
 }
