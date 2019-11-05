510323be078732fd2e09849ca8d5e011b7b5cd4a
chidakiyo <chidakiyo@gmail.com>
2019-11-05 19:16:37 +0900

    byte配列の長さ変更できるように go1.11でも動くように

--- go-memleak-check/lib/random_stream.go
+++ go-memleak-check/lib/random_stream.go
@@ -5,19 +5,29 @@ import (
 	"github.com/gin-gonic/gin"
 	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 func RandStream(g *gin.Context) {
 	c := g.Request.Context()
+
+	bit := uint64(0)
+	bitSt := g.Query("b")
+	if bitSt == "" {
+		bit = 8
+	} else {
+		bit, _ = strconv.ParseUint(bitSt, 10, 64)
+	}
+
 	var buf []byte
-	b := genBytes()
+	b := genBytes(bit)
 	buf = append(buf, *b...)
 	log.Info(c, "byte: %v", buf)
 	g.String(http.StatusOK, "%v", buf)
 }
 
-func genBytes() *[]byte {
-	token := make([]byte, 8*8*8*8*8)
+func genBytes(bit uint64) *[]byte {
+	token := make([]byte, 1 << bit)
 	rand.Read(token)
 	return &token
 }
--- go-memleak-check/lib/random_stream_test.go
+++ go-memleak-check/lib/random_stream_test.go
@@ -3,6 +3,6 @@ package lib
 import "testing"
 
 func Test_gen_byte(t *testing.T) {
-	b := genBytes()
+	b := genBytes(8)
 	t.Logf("%v", b)
 }
