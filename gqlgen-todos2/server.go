a3499693ac36876842ee335eea32357731e1fd7a
chidakiyo <chidakiyo@gmail.com>
2020-10-12 11:17:37 +0900

    デバッグしてみた

--- gqlgen-todos2/server.go
+++ gqlgen-todos2/server.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/99designs/gqlgen/graphql"
 	"log"
 	"net/http"
 	"os"
@@ -20,6 +24,38 @@ func main() {
 	}
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	srv.AroundFields(func(ctx context.Context, next graphql.Resolver) (res interface{}, err error) {
+		//a, _ := json.MarshalIndent(ctx, "", "  ")
+		//fmt.Printf("---------------------------------------------\n")
+		//fmt.Printf("%+v\n", ctx)
+		//fmt.Printf("---------------------------------------------\n")
+		return next(ctx)
+	})
+
+	srv.AroundOperations(func(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
+		//fmt.Printf("---------------------------------------------\n")
+		//fmt.Printf("%+v\n", ctx)
+		//fmt.Printf("---------------------------------------------\n")
+		resp := next(ctx)
+
+		return resp
+	})
+
+	srv.AroundResponses(func(ctx context.Context, next graphql.ResponseHandler) *graphql.Response {
+		resp := next(ctx)
+		fmt.Printf("---------------------------------------------\n")
+		fmt.Printf("%s\n", string(resp.Data))
+		fmt.Printf("---------------------------------------------\n")
+		s, _ := json.MarshalIndent(resp.Extensions, "", "  ")
+		fmt.Printf("%s\n", s)
+		fmt.Printf("---------------------------------------------\n")
+		oc := graphql.GetOperationContext(ctx)
+		fmt.Printf("%s\n", oc.RawQuery)
+
+
+		return resp
+	})
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
