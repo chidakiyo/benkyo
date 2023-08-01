07ca9bd6ff70f0b18ddc615bb4a04d19d965ac15
chidakiyo <chidakiyo@gmail.com>
2023-08-01 18:55:08 +0900

    ログのテスト

---
+++ gcplog-test/go.mod
@@ -0,0 +1,12 @@
+module github.com/chidakiyo/benkyo/gcplog-test
+
+go 1.19
+
+require github.com/chidakiyo/gcplog v0.0.0
+
+require (
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	go.opencensus.io v0.23.0 // indirect
+)
+
+replace github.com/chidakiyo/gcplog => ../../gcplog
--- gcplog-test/main.go
+++ gcplog-test/main.go
@@ -1,1 +1,42 @@
-package gcp_log_test
+package main
+
+import (
+	"cloud.google.com/go/compute/metadata"
+	"fmt"
+	log "github.com/chidakiyo/gcplog"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	route()
+	fmt.Println("Start")
+
+	p, err := metadata.ProjectID()
+	if err != nil {
+		panic(err)
+	}
+	log.Initialize("test", p)
+	panic(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func route() {
+	http.HandleFunc("/log1", log1)
+}
+
+func log1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello")
+	log.Structure.Infof(r.Context(), struct {
+		Typ     string `json:"@type"`
+		Message string `json:"message"`
+	}{
+		Typ:     "type.googleapis.com/google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent",
+		Message: fmt.Sprintf("%s : %s", "アラートです", time.Now().GoString()),
+	})
+	w.WriteHeader(http.StatusOK)
+}
