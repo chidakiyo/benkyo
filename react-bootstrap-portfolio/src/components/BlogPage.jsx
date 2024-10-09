1916accde7f856a86cbdf7fa581fdf3da2337444
chidakiyo <chidakiyo@gmail.com>
2024-10-09 11:35:00 +0900

    ポートフォリオっぽいもの試し

---
+++ react-bootstrap-portfolio/src/components/BlogPage.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div className='container'>
+      <h1>ブログ</h1>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque architecto, doloremque dolor qui natus sapiente nesciunt eum perferendis necessitatibus reiciendis ex, quasi non corrupti rerum facere aspernatur eveniet iste laborum, accusamus consectetur esse perspiciatis laboriosam! Nemo explicabo, aut inventore itaque in nihil. Iusto voluptatum cum quasi tenetur. A ex, officia similique quas reprehenderit illo at illum corrupti, ducimus voluptas repudiandae exercitationem sint voluptate molestias eligendi, neque maiores aliquam omnis. Ipsum sed veniam non. Commodi voluptatibus atque error impedit officia ut culpa tempore ex vel aliquam itaque aliquid suscipit amet saepe nulla, obcaecati sed dignissimos. Commodi voluptatibus dicta illo iste voluptas.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque architecto, doloremque dolor qui natus sapiente nesciunt eum perferendis necessitatibus reiciendis ex, quasi non corrupti rerum facere aspernatur eveniet iste laborum, accusamus consectetur esse perspiciatis laboriosam! Nemo explicabo, aut inventore itaque in nihil. Iusto voluptatum cum quasi tenetur. A ex, officia similique quas reprehenderit illo at illum corrupti, ducimus voluptas repudiandae exercitationem sint voluptate molestias eligendi, neque maiores aliquam omnis. Ipsum sed veniam non. Commodi voluptatibus atque error impedit officia ut culpa tempore ex vel aliquam itaque aliquid suscipit amet saepe nulla, obcaecati sed dignissimos. Commodi voluptatibus dicta illo iste voluptas.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque architecto, doloremque dolor qui natus sapiente nesciunt eum perferendis necessitatibus reiciendis ex, quasi non corrupti rerum facere aspernatur eveniet iste laborum, accusamus consectetur esse perspiciatis laboriosam! Nemo explicabo, aut inventore itaque in nihil. Iusto voluptatum cum quasi tenetur. A ex, officia similique quas reprehenderit illo at illum corrupti, ducimus voluptas repudiandae exercitationem sint voluptate molestias eligendi, neque maiores aliquam omnis. Ipsum sed veniam non. Commodi voluptatibus atque error impedit officia ut culpa tempore ex vel aliquam itaque aliquid suscipit amet saepe nulla, obcaecati sed dignissimos. Commodi voluptatibus dicta illo iste voluptas.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque architecto, doloremque dolor qui natus sapiente nesciunt eum perferendis necessitatibus reiciendis ex, quasi non corrupti rerum facere aspernatur eveniet iste laborum, accusamus consectetur esse perspiciatis laboriosam! Nemo explicabo, aut inventore itaque in nihil. Iusto voluptatum cum quasi tenetur. A ex, officia similique quas reprehenderit illo at illum corrupti, ducimus voluptas repudiandae exercitationem sint voluptate molestias eligendi, neque maiores aliquam omnis. Ipsum sed veniam non. Commodi voluptatibus atque error impedit officia ut culpa tempore ex vel aliquam itaque aliquid suscipit amet saepe nulla, obcaecati sed dignissimos. Commodi voluptatibus dicta illo iste voluptas.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque architecto, doloremque dolor qui natus sapiente nesciunt eum perferendis necessitatibus reiciendis ex, quasi non corrupti rerum facere aspernatur eveniet iste laborum, accusamus consectetur esse perspiciatis laboriosam! Nemo explicabo, aut inventore itaque in nihil. Iusto voluptatum cum quasi tenetur. A ex, officia similique quas reprehenderit illo at illum corrupti, ducimus voluptas repudiandae exercitationem sint voluptate molestias eligendi, neque maiores aliquam omnis. Ipsum sed veniam non. Commodi voluptatibus atque error impedit officia ut culpa tempore ex vel aliquam itaque aliquid suscipit amet saepe nulla, obcaecati sed dignissimos. Commodi voluptatibus dicta illo iste voluptas.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque architecto, doloremque dolor qui natus sapiente nesciunt eum perferendis necessitatibus reiciendis ex, quasi non corrupti rerum facere aspernatur eveniet iste laborum, accusamus consectetur esse perspiciatis laboriosam! Nemo explicabo, aut inventore itaque in nihil. Iusto voluptatum cum quasi tenetur. A ex, officia similique quas reprehenderit illo at illum corrupti, ducimus voluptas repudiandae exercitationem sint voluptate molestias eligendi, neque maiores aliquam omnis. Ipsum sed veniam non. Commodi voluptatibus atque error impedit officia ut culpa tempore ex vel aliquam itaque aliquid suscipit amet saepe nulla, obcaecati sed dignissimos. Commodi voluptatibus dicta illo iste voluptas.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque architecto, doloremque dolor qui natus sapiente nesciunt eum perferendis necessitatibus reiciendis ex, quasi non corrupti rerum facere aspernatur eveniet iste laborum, accusamus consectetur esse perspiciatis laboriosam! Nemo explicabo, aut inventore itaque in nihil. Iusto voluptatum cum quasi tenetur. A ex, officia similique quas reprehenderit illo at illum corrupti, ducimus voluptas repudiandae exercitationem sint voluptate molestias eligendi, neque maiores aliquam omnis. Ipsum sed veniam non. Commodi voluptatibus atque error impedit officia ut culpa tempore ex vel aliquam itaque aliquid suscipit amet saepe nulla, obcaecati sed dignissimos. Commodi voluptatibus dicta illo iste voluptas.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque architecto, doloremque dolor qui natus sapiente nesciunt eum perferendis necessitatibus reiciendis ex, quasi non corrupti rerum facere aspernatur eveniet iste laborum, accusamus consectetur esse perspiciatis laboriosam! Nemo explicabo, aut inventore itaque in nihil. Iusto voluptatum cum quasi tenetur. A ex, officia similique quas reprehenderit illo at illum corrupti, ducimus voluptas repudiandae exercitationem sint voluptate molestias eligendi, neque maiores aliquam omnis. Ipsum sed veniam non. Commodi voluptatibus atque error impedit officia ut culpa tempore ex vel aliquam itaque aliquid suscipit amet saepe nulla, obcaecati sed dignissimos. Commodi voluptatibus dicta illo iste voluptas.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque architecto, doloremque dolor qui natus sapiente nesciunt eum perferendis necessitatibus reiciendis ex, quasi non corrupti rerum facere aspernatur eveniet iste laborum, accusamus consectetur esse perspiciatis laboriosam! Nemo explicabo, aut inventore itaque in nihil. Iusto voluptatum cum quasi tenetur. A ex, officia similique quas reprehenderit illo at illum corrupti, ducimus voluptas repudiandae exercitationem sint voluptate molestias eligendi, neque maiores aliquam omnis. Ipsum sed veniam non. Commodi voluptatibus atque error impedit officia ut culpa tempore ex vel aliquam itaque aliquid suscipit amet saepe nulla, obcaecati sed dignissimos. Commodi voluptatibus dicta illo iste voluptas.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque architecto, doloremque dolor qui natus sapiente nesciunt eum perferendis necessitatibus reiciendis ex, quasi non corrupti rerum facere aspernatur eveniet iste laborum, accusamus consectetur esse perspiciatis laboriosam! Nemo explicabo, aut inventore itaque in nihil. Iusto voluptatum cum quasi tenetur. A ex, officia similique quas reprehenderit illo at illum corrupti, ducimus voluptas repudiandae exercitationem sint voluptate molestias eligendi, neque maiores aliquam omnis. Ipsum sed veniam non. Commodi voluptatibus atque error impedit officia ut culpa tempore ex vel aliquam itaque aliquid suscipit amet saepe nulla, obcaecati sed dignissimos. Commodi voluptatibus dicta illo iste voluptas.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque architecto, doloremque dolor qui natus sapiente nesciunt eum perferendis necessitatibus reiciendis ex, quasi non corrupti rerum facere aspernatur eveniet iste laborum, accusamus consectetur esse perspiciatis laboriosam! Nemo explicabo, aut inventore itaque in nihil. Iusto voluptatum cum quasi tenetur. A ex, officia similique quas reprehenderit illo at illum corrupti, ducimus voluptas repudiandae exercitationem sint voluptate molestias eligendi, neque maiores aliquam omnis. Ipsum sed veniam non. Commodi voluptatibus atque error impedit officia ut culpa tempore ex vel aliquam itaque aliquid suscipit amet saepe nulla, obcaecati sed dignissimos. Commodi voluptatibus dicta illo iste voluptas.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque architecto, doloremque dolor qui natus sapiente nesciunt eum perferendis necessitatibus reiciendis ex, quasi non corrupti rerum facere aspernatur eveniet iste laborum, accusamus consectetur esse perspiciatis laboriosam! Nemo explicabo, aut inventore itaque in nihil. Iusto voluptatum cum quasi tenetur. A ex, officia similique quas reprehenderit illo at illum corrupti, ducimus voluptas repudiandae exercitationem sint voluptate molestias eligendi, neque maiores aliquam omnis. Ipsum sed veniam non. Commodi voluptatibus atque error impedit officia ut culpa tempore ex vel aliquam itaque aliquid suscipit amet saepe nulla, obcaecati sed dignissimos. Commodi voluptatibus dicta illo iste voluptas.
+      </p>
+
+    </div>
+  )
+}
+
+export default BlogPage
---
+++ react-bootstrap-portfolio/src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'react-bootstrap'
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div>
+            home page
+        </div>
+    )
+}
+
+export default HomePage
