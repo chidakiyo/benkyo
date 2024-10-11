7afcba02dbcd1637026b5c1f010dde2aa9fd5f5b
chidakiyo <chidakiyo@gmail.com>
2024-10-11 18:04:46 +0900

    viteでlintが動くところまでベース部分

---
+++ react-vite-setup2/eslint.config.js
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginReactJSXRuntime from "eslint-plugin-react/configs/jsx-runtime.js"; // error対応
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReactJSXRuntime,
+];
+
---
+++ react-vite-setup2/src/components/TextInput.test.tsx
@@ -0,0 +1,17 @@
+import userEvent from "@testing-library/user-event"
+import TextInput from "./TextInput"
+import {render, screen} from "@testing-library/react"
+
+test("TextInput Component Test", async () => {
+    const user = userEvent.setup()
+
+    render(<TextInput />)
+
+    const textElement = screen.getByText("Entered Text:");
+    expect(textElement).toBeInTheDocument();
+
+    // ユーザの発火イベントをテストする
+    const inputElement = screen.getByLabelText("Text Input");
+    await user.type(inputElement, "Hello World");
+    expect(screen.getByText("Entered Text: Hello World")).toBeInTheDocument();
+})
